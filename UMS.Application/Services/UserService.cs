6ecb2f277978de82615c3cf9a5f0d7f3bbf32223
Vano Tsintsabadze <tsintsabadzevano@gmail.com>
2025-03-14 13:58:46 +0400

    Add related user existence check

--- UMS.Application/Services/UserService.cs
+++ UMS.Application/Services/UserService.cs
@@ -22,13 +22,21 @@ public class UserService : IUserService
     public async Task<UserResponseModel> Create(UserRequestModel user, CancellationToken cancellationToken)
     {
         var conflictingUser = await _userRepository.GetAsync(us => us.SocialNumber == user.SocialNumber, cancellationToken);
-
+        
         if (conflictingUser is not null)
             throw new ConflictException("User with such social number exists already");
 
         if (!IsEighteen(user.DateOfBirth))
             throw new BadRequestException("User has to be at least 18 years old");
-        
+
+        if (user.Relationships is not null)
+        {
+            var doRelatedUsersExist = await CheckRelatedUsersExist(user.Relationships, cancellationToken);
+            
+            if (!doRelatedUsersExist)
+                throw new BadRequestException("One or more users in the provided relationships do not exist");
+        }
+
         var entity = user.Adapt<User>();
         var response = await _userRepository.AddAsync(entity, cancellationToken);
 
@@ -44,5 +52,18 @@ public class UserService : IUserService
             age--;
     
         return age >= 18;
+    }
+
+    private async Task<bool> CheckRelatedUsersExist(ICollection<UserRelationshipDto> relationships, CancellationToken cancellationToken)
+    {
+        foreach (var relationship in relationships)
+        {
+            var user = await _userRepository.GetAsync(u => u.Id == relationship.RelatedUserId, cancellationToken);
+
+            if (user is null)
+                return false;
+        }
+        
+        return true;
     }
 }
