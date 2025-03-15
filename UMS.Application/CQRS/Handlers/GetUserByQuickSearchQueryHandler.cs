2e0184ece663846a65eb83dea9840e67f256e9d3
Vano Tsintsabadze <tsintsabadzevano@gmail.com>
2025-03-15 15:40:29 +0400

    Implement a query handler for grabbing users via a quick search feature

---
+++ UMS.Application/CQRS/Handlers/GetUserByQuickSearchQueryHandler.cs
@@ -0,0 +1,32 @@
+﻿using Mapster;
+using MediatR;
+using UMS.Application.CQRS.Queries.User;
+using UMS.Application.Exceptions;
+using UMS.Application.Interfaces.Repositories;
+using UMS.Application.Models.User;
+
+namespace UMS.Application.CQRS.Handlers;
+
+public class GetUserByQuickSearchQueryHandler : IRequestHandler<GetUserByQuickSearchQuery, ICollection<UserResponseModel>>
+{
+    private readonly IUserRepository _userRepository;
+
+    public GetUserByQuickSearchQueryHandler(IUserRepository userRepository)
+    {
+        _userRepository = userRepository;
+    }
+    
+    public async Task<ICollection<UserResponseModel>> Handle(GetUserByQuickSearchQuery request, CancellationToken cancellationToken)
+    {
+        if (!string.IsNullOrWhiteSpace(request.Query))
+            throw new BadRequestException("Query can not be empty");
+
+        var users = await _userRepository.GetUserByQueryLike(
+            request.Query,
+            request.Page ?? 1,
+            request.PageSize ?? 10,
+            cancellationToken);
+
+        return users.Adapt<ICollection<UserResponseModel>>();
+    }
+}
---
+++ UMS.Application/CQRS/Queries/User/GetUserByQuickSearchQuery.cs
@@ -0,0 +1,6 @@
+﻿using MediatR;
+using UMS.Application.Models.User;
+
+namespace UMS.Application.CQRS.Queries.User;
+
+public record GetUserByQuickSearchQuery(string Query, int? Page, int? PageSize) : IRequest<ICollection<UserResponseModel>>;
