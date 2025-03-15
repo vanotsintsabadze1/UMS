b7172ae63cbe4a833eb81cd96dd517e90e22e0aa
Vano Tsintsabadze <tsintsabadzevano@gmail.com>
2025-03-16 00:38:39 +0400

    Make the navigation propreties in UserRelationship model non-required

--- UMS.Domain/ValueObjects/UserRelationship.cs
+++ UMS.Domain/ValueObjects/UserRelationship.cs
@@ -6,10 +6,10 @@ namespace UMS.Domain.ValueObjects;
 public class UserRelationship
 {
     public required int UserId { get; set; }
-    public required User User { get; set; }
+    public User User { get; set; }
     
     public required int RelatedUserId { get; set; }
-    public required User RelatedUser { get; set; }
+    public User RelatedUser { get; set; }
     
     public required UserRelationshipTypes RelationshipType { get; set; }
 }
