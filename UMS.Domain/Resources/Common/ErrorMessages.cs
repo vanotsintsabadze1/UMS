9c80ccd10b3eb3afc7b17548483a7669012cecd2
Vano Tsintsabadze <tsintsabadzevano@gmail.com>
2025-03-16 18:12:20 +0400

    Add localization support and resource files

---
+++ UMS.Domain/Resources/Common/ErrorMessages.cs
@@ -0,0 +1,7 @@
+﻿namespace UMS.Domain.Resources;
+
+// The sole purpose of this class existing is that
+// IStringLocalizer requires a name of the file in the generic
+// And since the error messages are in the ErrorMessages file
+// there has to be a generic class named the same way
+public class ErrorMessages { }
